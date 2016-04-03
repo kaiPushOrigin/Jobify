fccc49962b5174d64b9a64aadba35f60f80923ec
kashifkai28 <kashiv.agnelite@gmail.com>
2016-04-02 21:23:45 -0300

    Fixed controllers

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,7 @@
 // var myApp =  angular.module("myApp", [
 //     "ngRoute"]).
 //     config(["$routeProvider", "$locationProvider", function($routeProvider, $locationProvider) {
-//         $routeProviasder.
+//         $routeProvider.
 //         when("/pageOne", {
 //             templateUrl: "templates/pageOne.html",
 //             controller: "pageOneController"
@@ -21,12 +21,26 @@
 
 var myApp = angular.module('myApp', [
 	'ngRoute']).
-	config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
-		$routeProvider.when('/pageOne', {templateUrl: 'templates/pageOne.html'});
-		$routeProvider.when('/pageTwo', {templateUrl: 'templates/pageTwo.html'});
-		$routeProvider.when('/home', {templateUrl: 'templates/home.html'});
-		$routeProvider.otherwise({redirectTo: '/home'});
+	config(['$routeProvider', '$locationProvider',
+	function($routeProvider, $locationProvider) {
+		$routeProvider.
+		when('/analysis', {
+			templateUrl: 'templates/analysis.html',
+			controller: 'analysisController'
+		}).
+		when('/pageTwo', {
+			templateUrl: 'templates/pageTwo.html'
+		}).
+		when('/home', {
+			templateUrl: 'templates/home.html'
+		}).
+		otherwise({
+			redirectTo: '/home'
+		});
 
 		$locationProvider.html5Mode({enabled: true, requireBase: false});
-
 	}]);
+
+// myApp.controller('first', function($scope) {
+//   console.log("Test");
+// });
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,14 +1,24 @@
-var meanApp = angular.module('meanApp', []);
-meanApp.controller('AppCtrl', ['$scope','$http', function ($scope, $http) {
-    // console.log('Hello, World from controller');
+// var jobify = angular.module('jobify', ['ngRoute']);
 
-    $scope.addPerson = function() {
-        console.log($scope.person.name);
-        $http.get('/contactList').success(function(response) {
-            console.log("Success");
-            $scope.answer = response;
-            console.log($scope.answer[0].PRICE);
-        });
-    };
+myApp.controller('analysisController', ['$scope','$http', function ($scope, $http) {
+    console.log('Hello, World from controller');
 
+    // $scope.addPerson = function() {
+    //     console.log($scope.person.name);
+    //     $http.get('/contactList').success(function(response) {
+    //         console.log("Success");
+    //         $scope.answer = response;
+    //         console.log($scope.answer[0].PRICE);
+    //     });
+    // };
 }]);
+
+// meanApp.controller('analysisController', ['$scope','$http', function ($scope, $http) {
+//     // console.log('Hello, World from controller');
+//
+//     console.log("Hi");
+// }]);
+
+// myApp.controller('analysisController', function($scope) {
+//   console.log("Test");
+// });
